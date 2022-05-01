aba935f00411f904780175bcd2d194317ca373b1
ayden610 <70598746+ayden610@users.noreply.github.com>
2022-05-01 19:31:32 -0400

    added processVote

--- src/ElectionData.java
+++ src/ElectionData.java
@@ -1,11 +1,15 @@
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Hashtable;
 
 class ElectionData {
     LinkedList<String> ballot = new LinkedList<String>();
     LinkedList<String> votes = new LinkedList<String>();
+    Hashtable<Integer, String> firstVotes = new Hashtable<>();
+    Hashtable<Integer, String> secondVotes = new Hashtable<>();
+    Hashtable<Integer, String> thirdVotes = new Hashtable<>();
     Scanner keyboard = new Scanner(System.in);
-
+    int i = 1;
     ElectionData() {
         this.ballot.add("Gompei");
         this.ballot.add("Husky");
@@ -20,10 +24,19 @@ class ElectionData {
 
     public void screen() {
         this.printBallot();
-        System.out.println("Who do you want to vote for?");
-        String candidate = keyboard.next();
-        votes.add(candidate);
-        System.out.println("You voted for " + candidate);
+        System.out.println("Select your first vote");
+        String candidate1 = keyboard.next();
+        firstVotes.put(i, candidate1);
+        System.out.println("You voted for " + candidate1 + " as your first choice");
+        System.out.println("Select your second vote");
+        String candidate2 = keyboard.next();
+        secondVotes.put(i, candidate2);
+        System.out.println("You voted for " + candidate2 + " as your second choice");
+        System.out.println("Select your third vote");
+        String candidate3 = keyboard.next();
+        thirdVotes.put(i, candidate3);
+        System.out.println("You voted for " + candidate3 + " as your third choice");
+        ++i;
     }
 
     public int countVotes(String forcand) {
@@ -33,5 +46,12 @@ class ElectionData {
                 numvotes = numvotes+1;
         }
         return numvotes;
+    }
+
+    public void processVote(String FirstVote, String SecondVote, String ThirdVote){
+        firstVotes.put(i, FirstVote);
+        secondVotes.put(i, SecondVote);
+        thirdVotes.put(i, ThirdVote);
+        ++i;
     }
 }
